428810b1f3dcadf2c31d12dd31b8776fd8c8308e
Irvan Surya Nugraha <irvansurya1@gmail.com>
2023-10-29 10:54:56 +0700

    Initial commit
    Generated by create-expo-app 2.1.1.

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View } from "react-native";
+import {
+  PressableScreen,
+  StackHookScreen,
+  StackScreen,
+  TouchableScreen,
+  StackParamScreen
+} from "./screens";
+
+const App = () => {
+  return (
+    <View style={{ paddingTop: 0, flex: 1 }}>
+      {/* Panggil Screen Komponen Di Sini */}
+    </View>
+  );
+};
+
+export default App;
